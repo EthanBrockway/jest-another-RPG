3689ce3d8058ad8a422bf434e426ba2523087376
Skronkie <ebrockway01@gmail.com>
2022-02-28 18:25:45 -0600

    commit

---
+++ __tests__/random.test.js
@@ -0,0 +1,18 @@
+const { expect, it } = require("@jest/globals");
+const { test } = require("picomatch");
+const Potion = require("../lib/Potion.js");
+
+it("creates a health potion object", () => {
+  const potion = new Potion("health");
+
+  expect(potion.name).toBe("health");
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
+it("creates a random potion object", () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
